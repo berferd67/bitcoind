fad7b8fd4f20f853804ed9ef928eac914ea9583f
Paul Guest <36819271+berferd67@users.noreply.github.com>
2018-03-24 21:41:25 +0000

    Update bitcoind_install.sh

--- bitcoind_install.sh
+++ bitcoind_install.sh
@@ -50,6 +50,7 @@ apt-get update && apt-get dist-upgrade && apt-get clean
 
 #Install dev tools
 apt-get install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python python3 net-tools libsodium-dev
+yum install -y gmp-devel sqlite3-devel
 
 #Install additional dependencies for development and testing
 apt-get install -y asciidoc valgrind python3-pip
